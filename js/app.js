136ad137781aae8fc90572115abe789ab92213e8
Marta <marta.anthony1@gmail.com>
2020-06-03 14:04:29 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 console.log('connected');
 
 let allCreatures = [];
@@ -16,12 +16,13 @@ Creature.prototype.render = function(){
 
   const myTemplate = $('#photo-template').html();
 
-  const $newSection = $(`<section>${myTemplate}</section>`);
+  const $newSection = $(`<section class=${this.keyword}>${myTemplate}</section>`);
 
   $newSection.find('h2').text(this.title);
   $newSection.find('p').text(this.description);
   $newSection.find('img').attr('src', this.image_url);
   $newSection.find('img').attr('alt', this.keyword);
+
 
   $('main').append($newSection);
 }
@@ -36,28 +37,42 @@ Creature.prototype.render = function(){
 
 
 
-
+// creating a drop down menu with unique keywords
 const keywordExtractor = () => {
   const keywords = [];
   allCreatures.forEach(creature => {
+    // looping through all the keywords. If a keyword does not exist in the keywords array, we put it in there
     if (!keywords.includes(creature.keyword)){
       keywords.push(creature.keyword)
     }
   })
-  keywords.forEach(creatureAppend => {
-    let keywordAppend = `<option value="${creatureAppend}">${creatureAppend}</option>`
-    $('select').append(keywordAppend);
 
+  keywords.forEach(keyword => {
+    let keywordAppend = `<option value="${keyword}">${keyword}</option>`
+    $('select').append(keywordAppend);
   })
 }
 
-
+// getting the data from json file and creating and rendering the objects, then calling the keyword function
 $.ajax('data/page1.json', {method: 'GET', dataType: 'JSON'})
   .then(horns => {
     horns.forEach(value => {
       new Creature(value).render();
       console.log(allCreatures);
     })
-  }).then(() =>{
     keywordExtractor();
   })
+
+// we need a filter function
+  // identify what was clicked on
+  // remove everything
+  // show only what was clicked on
+function filter(event) {
+  // find the thing that was clicked
+  let thingIClickedOn = $(this).val();
+  $('section').hide();
+  $(`.${thingIClickedOn}`).show();
+}
+
+$('#menu').on('change', filter);
+  
